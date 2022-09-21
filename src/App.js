79d6f8bdd1bf4015896b1da67c4848c1b79694d8
munna vail <98878071+devsayemmolla@users.noreply.github.com>
2022-09-21 09:37:42 +0600

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState, useSyncExternalStore } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>All Users</h1>
+
+      <LoadUsers></LoadUsers>
+    </div>
+  );
+}
+function LoadUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div className="usersContainer">
+      {users.map((user) => (
+        // <li>name: {user.name}</li>
+        <User
+          id={user.id}
+          name={user.name}
+          username={user.username}
+          email={user.email}
+          street={user.address.street}
+          city={user.address.city}
+        ></User>
+      ))}
     </div>
   );
 }
 
+function User(props) {
+  return (
+    <div className="userContainer">
+      <h1>{props.name}</h1>
+      <p>Id: {props.id}</p>
+      <p>User Name: {props.username}</p>
+      <p>Email: {props.email}</p>
+      <p>Street: {props.street}</p>
+      <p>City: {props.city}</p>
+    </div>
+  );
+}
 export default App;
